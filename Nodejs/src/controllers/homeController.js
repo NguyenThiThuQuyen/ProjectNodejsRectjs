25bd30e2978629e6f397de3c97e55530874afe3a
ThuQuyen <nttquyen1902@gmail.com>
2022-03-09 21:02:43 +0700

    add and edit user

--- Nodejs/src/controllers/homeController.js
+++ Nodejs/src/controllers/homeController.js
@@ -20,8 +20,49 @@ let postCRUD = async (req, res) => {
     console.log(message);
     return res.send('post crud from server')
 }
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    console.log('-----------------------');
+    console.log(data);
+    console.log('-----------------------');
+    // return res.send('display get crud from controller')
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        console.log('---------------------');
+        console.log(userData);
+        console.log('---------------------');
+        // return res.send('found a user')
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+
+    }
+    else {
+        return res.send('user not found')
+    }
+}
+
+let putCRUD = async(req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
